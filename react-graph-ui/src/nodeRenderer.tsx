4c5ae73573273b60b9c97000776f229fcace7eb1
Matt Kirby <MattJKirby@outlook.com>
2022-12-19 01:06:04 +0000

    Node renderer should add nodes to node store

--- react-graph-ui/src/nodeRenderer.tsx
+++ react-graph-ui/src/nodeRenderer.tsx
@@ -3,6 +3,7 @@ import { ComponentType, createElement, Key } from "react"
 import DefaultNode from "./defaultNode"
 import Draggable from "./draggable"
 import { EdgeRenderer } from "./edgeRenderer"
+import { useNodeStore } from "./stores/nodeStore"
 
 interface NodeRendererProps {
   nodeList?: any
@@ -10,7 +11,7 @@ interface NodeRendererProps {
   customNodeTypes?: any
 }
 export const NodeRenderer = ({nodeList, customNodeTypes, edgeList}: NodeRendererProps) => {
-
+  const addNode = useNodeStore((state) => state.addNode)
   const nodeTypes: { [key: string]: ComponentType<any> } = {default: DefaultNode}
 
   return (
