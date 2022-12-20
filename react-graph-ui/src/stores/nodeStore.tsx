d965c2dbdf1b5a9aaa62adfa517e3970128286a2
Matt Kirby <MattJKirby@outlook.com>
2022-12-20 19:03:33 +0100

    Added updateNodePosition method to NodeStore hook

--- react-graph-ui/src/stores/nodeStore.tsx
+++ react-graph-ui/src/stores/nodeStore.tsx
@@ -5,6 +5,7 @@ interface NodeState {
   nodes: Node[];
   addNode: (node: Node) => void;
   removeNode: (id: number) => void;
+  updateNodePosition: (id: number, x: number, y: number) => void;
 }
 
 export const useNodeStore = create<NodeState>((set) => ({
@@ -17,6 +18,16 @@ export const useNodeStore = create<NodeState>((set) => ({
   removeNode: (id: number) => {
     set((state) => ({
       nodes: state.nodes.filter((node) => node.id !== id),
+    }))
+  },
+  updateNodePosition: (id: number, x: number, y: number) => {
+    set((state) => ({
+      nodes: state.nodes.map(node => {
+        if(node.id === id){
+          return {...node, position: {x: x, y: y}}
+        }
+        return node
+      })
     }))
   }
 }))
