2ea8ee169c9a890bb342716bf62e14a14d6a65cf
Matt Kirby <MattJKirby@outlook.com>
2022-12-21 00:08:19 +0100

    Flow component now publishes nodeTypes to the store

--- react-graph-ui/src/flow.tsx
+++ react-graph-ui/src/flow.tsx
@@ -1,22 +1,37 @@
-import React, { useRef } from "react"
+import React, { ComponentType, useRef } from "react"
 import { useEffect } from "react"
 import { useNodeStore } from "./stores/nodeStore"
 import FlowZoom from "./zoom"
 import { INodeData } from "./Node/INodeData"
+import { useGraphStore } from "./stores/GraphStore"
 
 interface FlowProps {
   children: React.ReactNode;
-  nodes: INodeData[]
+  nodes: INodeData[];
+  nodeTypes: { [key: string]: ComponentType<any> };
 }
 
-export const Flow = ({children, nodes}: FlowProps) => {
+/**
+ * Graph entrypoint component
+ * @param param0 
+ * @returns 
+ */
+export const Flow = ({children, nodes, nodeTypes}: FlowProps) => {
   const nodesRef = useRef(useNodeStore.getState().nodes)
   const addNode = useNodeStore((state) => state.addNode)
-  
+  const setUserNodeTypes = useGraphStore((state) => state.setUserNodeTypes)
+
+
+  /**
+   * Enables the nodesRef to suscriube to state.nodes
+   */
   useEffect(() => useNodeStore.subscribe(
     state => (nodesRef.current = state.nodes)
   ), [])
 
+  /**
+   * When nodeData is modified, publish changes to the NodeStore
+   */
   useEffect(() => {
     nodes.forEach(node => {
       if(!nodesRef.current.some(n => n.id === node.id)){
@@ -24,6 +39,13 @@ export const Flow = ({children, nodes}: FlowProps) => {
       }
     })
   }, [addNode, nodes, nodesRef])
+
+  /**
+   * Publish nodeTypes to the store
+   */
+  useEffect(() => {
+    setUserNodeTypes(nodeTypes);
+    }, [setUserNodeTypes, nodeTypes]);
 
   return (
     <div style={{width: '100%', height: '100%', overflow: "hidden", position: "relative"}}>
