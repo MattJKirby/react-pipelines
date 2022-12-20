12f697a09a208444b00d976b28138089388aa296
Matt Kirby <MattJKirby@outlook.com>
2022-12-20 18:57:04 +0100

    Node now advertises position to zustand

--- react-graph-ui/src/defaultNode.tsx
+++ react-graph-ui/src/defaultNode.tsx
@@ -3,6 +3,7 @@ import styles from '../styles/flow-ui/DefaultNode.module.css'
 import { NodeOutput } from "./components/outputs/nodeOutput"
 import { createRef, useEffect, useRef, useState } from "react"
 import React from "react"
+import { useNodeStore } from "./stores/nodeStore"
 
 interface DefaultNodeProps {
   node: any
@@ -11,18 +12,18 @@ interface DefaultNodeProps {
 
 const DefaultNode = ({node}: DefaultNodeProps) => {
   const nodeRef = createRef<HTMLDivElement>()
-  const [position, setPosition] = useState<{x: number, y: number}>({x: 0, y: 0})
+  const nodes = useNodeStore((state) => state.nodes)
+  const updateStoredPosition = useNodeStore((state) => state.updateNodePosition)
+  
   
   const updatePosition = (x: number, y: number) => {
-    if(x !== position.x && y !== position.y){
-      setPosition({x: x, y: y})
+    if(x !== node.position?.x && y !== node.position?.y){
+      updateStoredPosition(node.id, x, y)
     }
   }
 
-
-
   return (
-    <Draggable key={node.id} setPosition={updatePosition}>
+    <Draggable key={node.id} initPosition={node.position} updatePosition={updatePosition}>
       <div className={styles.nodeContainer} ref={nodeRef}>
         {node.name}
       </div>
