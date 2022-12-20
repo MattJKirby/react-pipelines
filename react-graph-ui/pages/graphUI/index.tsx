22c595b0c7def929c87c1579d8637c00c69b61f6
Matt Kirby <MattJKirby@outlook.com>
2022-12-21 00:07:23 +0100

    NodeTypes now using memoisation

--- react-graph-ui/pages/graphUI/index.tsx
+++ react-graph-ui/pages/graphUI/index.tsx
@@ -1,32 +1,33 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 import FlowCanvas from "../../src/canvas"
 import Flow from "../../src/flow"
-import { useNodeStore } from "../../src/stores/nodeStore"
+import TestNode from "../../src/Node/TestNode"
+
+const initialNodeList = [
+  {id: 0, type: 'default', name: 'node1', position: {x: 50, y: 50}}, 
+  {id: 1, type: 'TestNode', name: 'Node2', position: {x: 100, y: 50}, outputs: ['Output 1']}]
+
+  const edgeList = [
+    {sourceNodeId: 0, sourceNodeOutput: 'Output 1', targetNodeId: 1, targetNodeInput: 'Input 1'}
+  ]
 
 /**
  * Page for displaing graph UI components
  * @returns 
  */
 const GraphUI = () => {
-  const initialNodeList = [
-    {id: 0, type: 'default', name: 'node1', position: {x: 50, y: 50}}, 
-    {id: 1, type: 'default', name: 'Node2', position: {x: 100, y: 50}, outputs: ['Output 1']}]
-
-    const edgeList = [
-      {sourceNodeId: 0, sourceNodeOutput: 'Output 1', targetNodeId: 1, targetNodeInput: 'Input 1'}
-    ]
-
   const [nodes, setNodes] = useState(initialNodeList)
+  const nodeTypes = useMemo(() => ({ TestNode: TestNode }), []);
 
   const newNode = () => {
-    const newNode = { id: nodes.length, type: 'default', name: 'asdf'}
+    const newNode = { id: nodes.length, type: 'default', name: 'asdf', position: {x: 300, y: 40}}
     setNodes([...nodes, newNode])
   }
 
   return (
     <div style={{flex: '1', overflow: "hidden", margin: "5rem", height: "500px"}}>
       <button onClick={newNode}>Add Node</button> 
-      <Flow nodes={nodes}>
+      <Flow nodes={nodes} nodeTypes={nodeTypes}>
         <FlowCanvas gap={40} size={1} />
       </Flow>    
     </div>
