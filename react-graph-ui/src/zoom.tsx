fc95ab971e663347b6f14e738428ae8374d99219
Matt Kirby <MattJKirby@outlook.com>
2022-12-20 19:02:53 +0100

    zoom now updates context store

--- react-graph-ui/src/zoom.tsx
+++ react-graph-ui/src/zoom.tsx
@@ -1,27 +1,36 @@
-import { Children, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import * as d3 from "d3-zoom";
-import {create,select} from 'd3-selection'
-import FlowCanvas from "./canvas";
+import {select} from 'd3-selection'
 import NodeRenderer from "./nodeRenderer";
 import React from "react";
 import useTransformStore from "./stores/transformStore";
+import { useZoomContextStore } from "./stores/zoomContextStore";
 
 interface FlowZoomProps {
   children?: React.ReactNode
-  nodeList: any
-  edgeList: any
-  setTransform: (transform: any) => void
 }
 
-
-export const FlowZoom = ({children, nodeList, edgeList, setTransform}: FlowZoomProps) => {
+export const FlowZoom = ({children}: FlowZoomProps) => {
   const flowZoom = useRef<HTMLDivElement>(null);
+  const zoomContext = useRef<HTMLDivElement>(null);
   const scale = useTransformStore((state) => state.scale);
   const translateX = useTransformStore((state) => state.translateX);
   const translateY = useTransformStore((state) => state.translateY);
   const setScale = useTransformStore((state) => state.setScale);
   const setTranslateX = useTransformStore((state) => state.setTranslateX);
   const setTranslateY = useTransformStore((state) => state.setTranslateY);
+  const setZoomContextDimensions = useZoomContextStore((state) => state.setContextDimensions);
+  const setZoomContextPosition = useZoomContextStore((state) => state.setContextPosition);
+
+  const updateZoomContext = useCallback(() => {
+    const context = zoomContext.current
+    if(context !== null && flowZoom.current !== null){
+      const contextRect = context.getBoundingClientRect();
+      const zoomRect = flowZoom.current.getBoundingClientRect();
+      setZoomContextDimensions({width: contextRect.width, height: contextRect.height})
+      setZoomContextPosition({offsetX: (contextRect.left - zoomRect.left) / scale, offsetY: (contextRect.top - zoomRect.top) / scale})
+    }
+  }, [scale, setZoomContextDimensions, setZoomContextPosition]);
 
   useEffect(() => {
     if(flowZoom.current){
@@ -35,9 +44,10 @@ export const FlowZoom = ({children, nodeList, edgeList, setTransform}: FlowZoomP
         event.sourceEvent.stopPropagation();
       }).scaleExtent([0.1, 2]).filter(event => zoomFilter(event));
       select(flowZoom.current as Element).call(zoom);
+      updateZoomContext()
     }
     
-  }, [scale, setScale, setTranslateX, setTranslateY, translateX, translateY])
+  }, [scale, setScale, setTranslateX, setTranslateY, translateX, translateY, updateZoomContext])
 
   const zoomFilter = (e) => {
     const className = e.target.classList.contains('flow-ui-noZoom')
@@ -45,12 +55,11 @@ export const FlowZoom = ({children, nodeList, edgeList, setTransform}: FlowZoomP
   }
 
   return (
-    <div className='' ref={flowZoom} style={{ width: '100%', height: '100%', zIndex: -1, backgroundColor: 'transparent'}}>
-      <div style={{ height: "100%", left: 0, top: 0, width: "100%", transform: `translate(${translateX}px, ${translateY}px) scale(${scale})`, transformOrigin: '0 0'}}>
-        <NodeRenderer nodeList={nodeList}/>
+    <div ref={flowZoom} style={{ width: '100%', height: '100%', zIndex: -1, backgroundColor: 'transparent'}}>
+      <div ref={zoomContext} style={{ height: "100%", left: 0, top: 0, width: "100%", transform: `translate(${translateX}px, ${translateY}px) scale(${scale})`, transformOrigin: '0 0'}}>
+        <NodeRenderer />
         {children}
       </div>
-      
     </div>
   );
 }
