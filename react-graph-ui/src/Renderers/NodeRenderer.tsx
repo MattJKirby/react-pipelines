b08166e351b89ce9634de52f7a82944edd3c224f
Matt Kirby <MattJKirby@outlook.com>
2022-12-22 23:23:31 +0100

    Added NodeType Props instead of any

--- react-graph-ui/src/Renderers/NodeRenderer.tsx
+++ react-graph-ui/src/Renderers/NodeRenderer.tsx
@@ -1,21 +1,25 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { ComponentType } from "react"
 import NodeContainer from "../Containers/NodeContainer"
-import { EdgeRenderer } from "./EdgeRenderer"
 import { useNodeStore } from "../Stores/NodeStore"
 import DefaultNode from "../Components/Node/DefaultNode"
 import { useGraphStore } from "../Stores/GraphStore"
+import { INodeData } from "../Components/Node/INodeData"
+
+export interface NodeTypeProps {
+  nodeData: INodeData
+}
 
 
 export const NodeRenderer = () => {
   const nodes = useNodeStore((state) => state.nodes);
   const userNodeTypes = useGraphStore((state) => state.userNodeTypes)
-  const nodeTypes: { [key: string]: ComponentType<any> } = {...{default: DefaultNode}, ...userNodeTypes}
+  const nodeTypes: { [key: string]: ComponentType<NodeTypeProps> } = {...{default: DefaultNode}, ...userNodeTypes}
 
   return (
     <div style={{position: 'relative', display: 'flex'}}>
         {nodes.map(node => {
-          const NodeType = nodeTypes[node.type] as ComponentType<any> || nodeTypes['default']
+          const NodeType = nodeTypes[node.type] as ComponentType<NodeTypeProps> || nodeTypes['default']
           
           return (
             <NodeContainer key={node.id} nodeData={node}>
--- react-graph-ui/src/stores/GraphStore.tsx
+++ react-graph-ui/src/stores/GraphStore.tsx
@@ -1,12 +1,13 @@
 import { ComponentType } from 'react'
 import create from 'zustand'
+import { NodeTypeProps } from '../Renderers/NodeRenderer';
 
 type IGraphStore = {
-  userNodeTypes: { [key: string]: ComponentType<any> };
-  setUserNodeTypes: (nodeTypes: { [key: string]: ComponentType<any> }) => void;
+  userNodeTypes: { [key: string]: ComponentType<NodeTypeProps> };
+  setUserNodeTypes: (nodeTypes: { [key: string]: ComponentType<NodeTypeProps> }) => void;
 }
 
 export const useGraphStore = create<IGraphStore>((set) => ({
   userNodeTypes: {},
-  setUserNodeTypes: (userNodeTypes: { [key: string]: ComponentType<any> }) => set({userNodeTypes})
+  setUserNodeTypes: (userNodeTypes: { [key: string]: ComponentType<NodeTypeProps> }) => set({ userNodeTypes })
 }))
