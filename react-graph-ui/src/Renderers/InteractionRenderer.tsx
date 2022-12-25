274945600bb75e5bb330f813da3c9e573ecdff42
Matt Kirby <MattJKirby@outlook.com>
2022-12-25 23:38:06 +0100

    Added interaction Renderer

---
+++ react-graph-ui/src/Renderers/InteractionRenderer.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+import { IHandleData } from "../Components/Handles/IHandleData"
+import { useInteractionStore } from "../Stores/InteractionStore"
+import { useNodeIOStore } from "../Stores/NodeIOStore"
+
+/**
+ * Interaction renderer used to render all user-graph interactions
+ * @returns 
+ */
+export const InteractionRenderer = () => {
+  const getHandle = useNodeIOStore((state) => state.getHandle)
+  const [edgeInteractionSourceHandle, setEdgeInteractionSourceHandle] = useState<IHandleData | undefined>(undefined)
+  const [edgeInteractionTargetPosition, setEdgeInteractionTargetPosition] = useState<undefined | {x: number, y: number} >(undefined)
+  const edgeInteraction = useInteractionStore((state) => state.edgeInteraction)
+
+  useEffect(() => {
+    if(edgeInteraction && edgeInteraction.mousePosition){
+      setEdgeInteractionSourceHandle(getHandle(edgeInteraction.sourceNodeId, edgeInteraction.sourceHandleId))
+      setEdgeInteractionTargetPosition(edgeInteraction.mousePosition)
+      return
+    }
+    setEdgeInteractionTargetPosition(undefined)
+  }, [edgeInteraction, getHandle])
+
+  return (
+    <svg width={'100%'} height={'100%'} overflow="visible" style={{position: "absolute"}}>
+      {edgeInteractionTargetPosition && 
+        <path 
+          d={`M${edgeInteractionSourceHandle?.position.x} ${edgeInteractionSourceHandle?.position.y} L ${edgeInteractionTargetPosition.x} ${edgeInteractionTargetPosition.y}`} 
+          style={{stroke: '#bbb'}}
+        />}
+    </svg>
+  )
+}
