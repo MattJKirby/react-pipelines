eb0a4b7c7126ed576339846cce23955d629dac1b
Matt Kirby <MattJKirby@outlook.com>
2022-12-27 14:37:12 +0100

    Renmawd edges to edgeDataList

--- react-graph-ui/src/Contexts/EdgeDataContext.tsx
+++ react-graph-ui/src/Contexts/EdgeDataContext.tsx
@@ -1,9 +1,10 @@
 import { createContext, useContext } from 'react';
+import { IEdgeData } from '../Components/Edge/IEdgeData';
 import { IHandleData } from '../Components/Handle/IHandleData';
 
-export const EdgeDataContext = createContext<{source: IHandleData, target: IHandleData} | undefined>(undefined);
+export const EdgeDataContext = createContext<{edge: IEdgeData, source: IHandleData, target: IHandleData} | undefined>(undefined);
 
-export const useEdgeContext = (): {source: IHandleData, target: IHandleData} | undefined => {
+export const useEdgeContext = (): {edge: IEdgeData, source: IHandleData, target: IHandleData} | undefined => {
   return useContext(EdgeDataContext);
 };
 
