848f58b4d3d829aef96b5830ea43e08a1f09fb98
Matt Kirby <MattJKirby@outlook.com>
2022-12-27 14:36:56 +0100

    Edge renderer refactor

--- react-graph-ui/src/Renderers/EdgeRenderer.tsx
+++ react-graph-ui/src/Renderers/EdgeRenderer.tsx
@@ -1,9 +1,12 @@
 import React, { ComponentType, useEffect } from "react"
 import { Edge } from "../Components/Edge"
 import DefaultEdge from "../Components/Edge/DefaultEdge"
+import { IEdgeData } from "../Components/Edge/IEdgeData"
+import { IHandleData } from "../Components/Handle/IHandleData"
 import { useEdgeStore } from "../Stores/EdgeStore"
 import { useGraphStore } from "../Stores/GraphStore"
 import { useInteractionStore } from "../Stores/InteractionStore"
+import { useNodeIOStore } from "../Stores/NodeIOStore"
 
 
 
@@ -14,34 +17,40 @@ export interface EdgeTypeProps {
 export const EdgeRenderer = () => {
   const userEdgeTypes = useGraphStore((state) => state.userEdgeTypes)
   const edgeTypes: { [key: string]: ComponentType<EdgeTypeProps> } = {...{default: DefaultEdge}, ...userEdgeTypes}
-
-  const {edges, newEdge} = useEdgeStore()
+  const nodeHandles = useNodeIOStore((state) => state.nodeHandles)
+  const {edgeDataList, newEdge} = useEdgeStore()
   const {edgeInteraction, resetEdgeInteraction} = useInteractionStore()
-
-
-
 
   useEffect(() => {
     if(edgeInteraction?.targetNodeId !== undefined && edgeInteraction.targetHandleId !== undefined){
-      const edge = edges.find(e => e.sourceNodeId === edgeInteraction.sourceNodeId && e.sourceNodeOutput === edgeInteraction.sourceHandleId && e.targetNodeId === edgeInteraction.targetNodeId && e.targetNodeInput === edgeInteraction.targetHandleId)
-      if(edge === undefined){
+      const existingEdge = edgeDataList.find(e => e.sourceNodeId === edgeInteraction.sourceNodeId && e.sourceNodeOutput === edgeInteraction.sourceHandleId && e.targetNodeId === edgeInteraction.targetNodeId && e.targetNodeInput === edgeInteraction.targetHandleId)
+      if(existingEdge === undefined){
+        console.log("ADDED EDGE")
         newEdge(edgeInteraction.sourceNodeId, edgeInteraction.sourceHandleId, edgeInteraction.targetNodeId, edgeInteraction.targetHandleId)
       }
       resetEdgeInteraction()
     }
-  }, [edgeInteraction?.sourceHandleId, edgeInteraction?.sourceNodeId, edgeInteraction?.targetHandleId, edgeInteraction?.targetNodeId, edges, newEdge, resetEdgeInteraction])
+  }, [edgeInteraction, edgeDataList, newEdge, resetEdgeInteraction])
+
+  const getNodeHandle = (nodeId: number, handleId: string): IHandleData | undefined => {
+    return nodeHandles.find(h => h.nodeId === nodeId && h.id === handleId)
+  }
 
 
   return (
     <svg width={'100%'} height={'100%'} overflow="visible" style={{position: "absolute"}}>
-      {edges.map((edge, index) => {
+      {edgeDataList.map((edge, index) => {
         const EdgeType = edgeTypes[edge.type] as ComponentType<EdgeTypeProps> || edgeTypes['default']
+        const source = getNodeHandle(edge.sourceNodeId, edge.sourceNodeOutput)
+        const target = getNodeHandle(edge.targetNodeId, edge.targetNodeInput)
 
+      if(source && target){
         return (
-          <Edge key={index} edge={edge}>
+          <Edge key={index} edge={edge} source={source} target={target}>
             <EdgeType />
           </Edge>
         )
+      }
       })}
     </svg>
   )
