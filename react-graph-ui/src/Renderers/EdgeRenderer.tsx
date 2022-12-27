2d71f8e371ee3ede40bbefe9b92d1908c205a0df
Matt Kirby <MattJKirby@outlook.com>
2022-12-27 02:06:43 +0100

    Removed old edgeRenderer UseEffect hooks

--- react-graph-ui/src/Renderers/EdgeRenderer.tsx
+++ react-graph-ui/src/Renderers/EdgeRenderer.tsx
@@ -1,51 +1,25 @@
-import React, { useEffect, useState } from "react"
+import React, { ComponentType, useEffect } from "react"
+import { Edge } from "../Components/Edge"
+import DefaultEdge from "../Components/Edge/DefaultEdge"
 import { useEdgeStore } from "../Stores/EdgeStore"
+import { useGraphStore } from "../Stores/GraphStore"
 import { useInteractionStore } from "../Stores/InteractionStore"
-import { useNodeIOStore } from "../Stores/NodeIOStore"
 
-interface EdgeCoordinate {
-  edgeId: string
-  source: {x: number, y: number}
-  target: {x: number, y: number}
+
+
+export interface EdgeTypeProps {
 }
 
 
 export const EdgeRenderer = () => {
-  const nodeHandles = useNodeIOStore((state) => state.nodeHandles)
+  const userEdgeTypes = useGraphStore((state) => state.userEdgeTypes)
+  const edgeTypes: { [key: string]: ComponentType<EdgeTypeProps> } = {...{default: DefaultEdge}, ...userEdgeTypes}
+
   const {edges, newEdge} = useEdgeStore()
-  const {dragInteractionNodeId, edgeInteraction, resetEdgeInteraction} = useInteractionStore()
-  const [edgeCoordinates, setEdgeCoordinates] = useState<EdgeCoordinate[]>([])
+  const {edgeInteraction, resetEdgeInteraction} = useInteractionStore()
 
-  useEffect(() => {
-    edges.forEach(edge => {
-      if(edgeCoordinates.find(e => e.edgeId === edge.id) === undefined){     
-        const sourceHandlePosition = nodeHandles.find(s => s.nodeId === edge.sourceNodeId && s.id === edge.sourceNodeOutput)?.position
-        const targetHandlePosition = nodeHandles.find(t => t.nodeId === edge.targetNodeId && t.id === edge.targetNodeInput)?.position
-       
-        if(sourceHandlePosition !== undefined && targetHandlePosition !== undefined) {
-          setEdgeCoordinates([...edgeCoordinates, {edgeId: edge.id, source: sourceHandlePosition, target: targetHandlePosition}])
-        }
-      }
-    })
-  }, [edgeCoordinates, edgeInteraction, edges, nodeHandles])
 
-  useEffect(() => {
-    if(dragInteractionNodeId !== undefined){
-      const connectedEdges = edges.filter(e => e.sourceNodeId === dragInteractionNodeId || e.targetNodeId === dragInteractionNodeId)
 
-      connectedEdges.forEach(edge => {
-        const edgeCoordinate = edgeCoordinates.find(e => e.edgeId === edge.id)
-        const sourceHandlePosition = nodeHandles.find(s => s.nodeId === edge.sourceNodeId && s.id === edge.sourceNodeOutput)?.position
-        const targetHandlePosition = nodeHandles.find(t => t.nodeId === edge.targetNodeId && t.id === edge.targetNodeInput)?.position
-          
-        if(edgeCoordinate?.source !== sourceHandlePosition || edgeCoordinate?.target !== targetHandlePosition){
-          if(sourceHandlePosition && targetHandlePosition !== undefined && edgeCoordinate !== undefined){
-            setEdgeCoordinates([...edgeCoordinates.filter(e => e.edgeId !== edge.id), {...edgeCoordinate, source: sourceHandlePosition, target: targetHandlePosition}])
-          }
-        }
-      })
-    }
-  }, [dragInteractionNodeId, edgeCoordinates, edges, nodeHandles])
 
   useEffect(() => {
     if(edgeInteraction?.targetNodeId !== undefined && edgeInteraction.targetHandleId !== undefined){
@@ -60,19 +34,17 @@ export const EdgeRenderer = () => {
 
   return (
     <svg width={'100%'} height={'100%'} overflow="visible" style={{position: "absolute"}}>
-      {edgeCoordinates.map((edge, index) => {
-        
+      {edges.map((edge, index) => {
+        const EdgeType = edgeTypes[edge.type] as ComponentType<EdgeTypeProps> || edgeTypes['default']
+
         return (
-          <g 
-            key={index} 
-            style={{border: "1px solid red", fill: "red"}}
-            onClick={() => console.log("ASDFG")} 
-            >
-            <path d={`M${edge.source.x} ${edge.source.y} L ${edge.target.x} ${edge.target.y}`} style={{stroke: '#bbb'}}/>
-          </g>
-          
+          <Edge key={index} edge={edge}>
+            <EdgeType />
+          </Edge>
         )
       })}
     </svg>
   )
 }
+
+export default EdgeRenderer
