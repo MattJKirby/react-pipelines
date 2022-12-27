651000481234b05165932d5bec32bb5d8e97cf78
Matt Kirby <MattJKirby@outlook.com>
2022-12-27 14:34:13 +0100

    Edge only re-renders when source or target handles change

--- react-graph-ui/src/components/Edge/index.tsx
+++ react-graph-ui/src/components/Edge/index.tsx
@@ -1,28 +1,26 @@
 import { useEffect, useState } from "react"
 import EdgeDataContext from "../../Contexts/EdgeDataContext"
-import { useNodeIOStore } from "../../Stores/NodeIOStore"
 import { IHandleData } from "../Handle/IHandleData"
 import { IEdgeData } from "./IEdgeData"
 
 interface EdgeProps {
   children: React.ReactNode
   edge: IEdgeData
+  source: IHandleData
+  target: IHandleData
 }
 
-export const Edge = ({children, edge}: EdgeProps) => {
-  const nodeHandles = useNodeIOStore((state) => state.nodeHandles)
-  const [edgeHandles, setEdgeHandles] = useState<{source: IHandleData, target: IHandleData}>()
+export const Edge = ({children, edge, source, target}: EdgeProps) => {
+  const [edgeHandles, setEdgeHandles] = useState<{source: IHandleData, target: IHandleData}>({source, target})
 
   useEffect(() => {
-    const source = nodeHandles.find(s => s.nodeId === edge.sourceNodeId && s.id === edge.sourceNodeOutput)
-    const target = nodeHandles.find(t => t.nodeId === edge.targetNodeId && t.id === edge.targetNodeInput)
     if(source && target){
       setEdgeHandles({source, target})
     }
-  }, [edge.sourceNodeId, edge.sourceNodeOutput, edge.targetNodeId, edge.targetNodeInput, nodeHandles])
+  }, [source, target, setEdgeHandles, edge.id])
 
   return (
-   <EdgeDataContext.Provider value={edgeHandles}>
+   <EdgeDataContext.Provider value={{edge, ...edgeHandles}}>
     {children}
    </EdgeDataContext.Provider>
   )
