cbd52d1e0529dc97a0f69ae68efa143819770faa
Matt Kirby <MattJKirby@outlook.com>
2022-12-27 14:37:27 +0100

    Renamed edges to edgeDataList

--- react-graph-ui/src/stores/EdgeStore.tsx
+++ react-graph-ui/src/stores/EdgeStore.tsx
@@ -2,21 +2,21 @@ import create from "zustand";
 import { IEdgeData } from "../Components/Edge/IEdgeData";
 
 interface EdgeState {
-  edges: IEdgeData[]
+  edgeDataList: IEdgeData[]
   addEdge: (edge: IEdgeData) => void
   newEdge: (sourceNodeId: number, sourceNodeOutput: string, targetNodeId: number, targetNodeInput: string, type: string) => void
 }
 
 export const useEdgeStore = create<EdgeState>((set) => ({
-  edges: [],
+  edgeDataList: [],
   addEdge: (edge: IEdgeData) => {
     set((state) => ({
-      edges: [...state.edges, edge],
+      edgeDataList: [...state.edgeDataList, edge],
     }))
   },
   newEdge: (sourceNodeId: number, sourceNodeOutput: string, targetNodeId: number, targetNodeInput: string, type: string) => {
     set((state) => ({
-      edges: [...state.edges, {id:`edge-${state.edges.length}-${sourceNodeId}-${targetNodeId}`, sourceNodeId, sourceNodeOutput, targetNodeId, targetNodeInput, type}]
+      edgeDataList: [...state.edgeDataList, {id:`edge-${state.edgeDataList.length}-${sourceNodeId}-${targetNodeId}`, sourceNodeId, sourceNodeOutput, targetNodeId, targetNodeInput, type}]
     }))
   }
 }))
