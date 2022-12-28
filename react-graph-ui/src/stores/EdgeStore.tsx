174e9bc29c4e5c5724a7d635390c4ee8e767a2d0
Matt Kirby <MattJKirby@outlook.com>
2022-12-28 17:16:19 +0100

    New getEdge method on edgeStore

--- react-graph-ui/src/stores/EdgeStore.tsx
+++ react-graph-ui/src/stores/EdgeStore.tsx
@@ -5,9 +5,10 @@ interface EdgeState {
   edgeDataList: IEdgeData[]
   addEdge: (edge: IEdgeData) => void
   newEdge: (sourceNodeId: number, sourceNodeOutput: string, targetNodeId: number, targetNodeInput: string, type: string) => void
+  getEdge: (sourceNodeId: number, nodeOutput: string, targetNodeId: number, nodeInput: string) => IEdgeData | undefined
 }
 
-export const useEdgeStore = create<EdgeState>((set) => ({
+export const useEdgeStore = create<EdgeState>((set, get) => ({
   edgeDataList: [],
   addEdge: (edge: IEdgeData) => {
     set((state) => ({
@@ -18,5 +19,8 @@ export const useEdgeStore = create<EdgeState>((set) => ({
     set((state) => ({
       edgeDataList: [...state.edgeDataList, {id:`edge-${state.edgeDataList.length}-${sourceNodeId}-${targetNodeId}`, sourceNodeId, sourceNodeOutput, targetNodeId, targetNodeInput, type}]
     }))
+  },
+  getEdge: (sourceNodeId: number, nodeOutput: string, targetNodeId: number, nodeInput: string) => {
+    return get().edgeDataList.find(e => e.sourceNodeId === sourceNodeId && e.sourceNodeOutput === nodeOutput && e.targetNodeId === targetNodeId && e.targetNodeInput === nodeInput)
   }
 }))
