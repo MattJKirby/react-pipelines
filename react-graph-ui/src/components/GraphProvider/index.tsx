33480ffb8238954a24a717d0d9af034a72a5ca1d
Matt Kirby <MattJKirby@outlook.com>
2022-12-30 14:56:22 +0100

    Added GraphStoreProvider react context

---
+++ react-graph-ui/src/components/GraphProvider/index.tsx
@@ -0,0 +1,30 @@
+import { useRef } from "react";
+import { StoreApi } from "zustand";
+import GraphStoreContext from "../../Contexts/GraphStoreContext";
+import { createGraphStore } from "../../Store";
+import { IGraphState } from "../../Types";
+
+type GraphProviderProps = React.PropsWithChildren
+
+/**
+ * GraphProvider component enables multiple graphs on each page to have their own GraphStore instance, meaning data is not shared between graphs.
+ * This provider also enables the GraphStore to be accesses outside of the Graph component, required for external functionality that reads and/or mutates graphStore instance. 
+ * @param param0 
+ * @returns 
+ */
+const GraphProvider = ({ children }: GraphProviderProps) => {
+  const storeRef = useRef<StoreApi<IGraphState> | null>(null);
+
+  if (!storeRef.current) {
+    storeRef.current = createGraphStore();
+  }
+
+  return (
+    <GraphStoreContext.Provider value={storeRef.current}>
+      {children}
+    </GraphStoreContext.Provider>
+  );
+};
+
+GraphProvider.displayName = 'GraphProvider';
+export default GraphProvider;
