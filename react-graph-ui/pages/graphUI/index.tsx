1b999d0ed1c773c3a655b5fd946afb303d607f4a
Matt Kirby <MattJKirby@outlook.com>
2023-01-01 23:10:31 +0100

    multiple graph provider instances

--- react-graph-ui/pages/graphUI/index.tsx
+++ react-graph-ui/pages/graphUI/index.tsx
@@ -5,14 +5,14 @@ import GraphProvider from "../../src/Components/GraphProvider"
 import TestNode from "../../src/Components/Node/TestNode"
 
 const initialNodeList = [
-  {id: '0', type: 'default', name: 'node1', position: {x: 50, y: 50}}, 
-  {id: '1', type: 'TestNode', name: 'Node2', position: {x: 100, y: 50}},
-  {id: '2', type: 'default', name: 'node3', position: {x: 50, y: 100}}, 
+  {id: '0', type: 'default', name: 'node0', position: {x: 50, y: 50}}, 
+  {id: '1', type: 'TestNode', name: 'Node1', position: {x: 100, y: 50}},
+  {id: '2', type: 'default', name: 'node2', position: {x: 50, y: 100}}, 
 ]
 
   const initialEdgeList = [
-    {id: "edge-1", sourceNodeId: 0, sourceNodeOutput: 'source', targetNodeId: 1, targetNodeInput: 'target', type: 'default'},
-    {id: "edge-2", sourceNodeId: 2, sourceNodeOutput: 'source', targetNodeId: 1, targetNodeInput: 'target', type: 'default'}
+    {id: "edge-0-1", sourceNodeId: '0', sourceNodeOutput: 'source', targetNodeId: '1', targetNodeInput: 'target', type: 'default'},
+    {id: "edge-2-1", sourceNodeId: '2', sourceNodeOutput: 'source', targetNodeId: '1', targetNodeInput: 'target', type: 'default'}
   ]
 
 /**
@@ -39,14 +39,14 @@ const GraphUI = () => {
       </GraphProvider>
     </div>
 
-    <div style={{flex: '1', overflow: "hidden", margin: "5rem", height: "500px"}}>
+     <div style={{flex: '1', overflow: "hidden", margin: "5rem", height: "500px"}}>
       <button onClick={newNode}>Add Node</button> 
       <GraphProvider>
         <Graph nodes={nodes} nodeTypes={nodeTypes} edges={edges}>
           <GraphCanvas gap={40} size={1} />
         </Graph>
       </GraphProvider>
-    </div>
+    </div> 
 
   </div>
     
