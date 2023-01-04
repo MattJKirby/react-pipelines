a1372d6691c8b24f4b3d70b7a8f08ad75d47b33c
Matt Kirby <MattJKirby@outlook.com>
2023-01-04 18:43:55 +0100

    Added new enableDraggableNodes store prop

--- react-graph-ui/src/Store/initialState.ts
+++ react-graph-ui/src/Store/initialState.ts
@@ -1,8 +1,9 @@
-import { IGraphStoreProps } from "../Types";
+import { IGraphStore } from "../Types";
 
-export const initialGraphState: IGraphStoreProps = {
+export const initialGraphState: IGraphStore = {
   graphId: "1",
   graphTransform: { scale: 1, translateX: 0, translateY: 0 },
+  enableDraggableNodes: true,
   nodes: [],
   edges: [],
   customNodeTypes: {},
