0d8cc6c6a808234cef1da61e916da3b8cde6604a
Matt Kirby <MattJKirby@outlook.com>
2023-01-04 18:42:38 +0100

    Removed ugly store setters

--- react-graph-ui/src/components/Graph/index.tsx
+++ react-graph-ui/src/components/Graph/index.tsx
@@ -1,20 +1,13 @@
-import React, { ComponentType, useRef, MouseEvent } from "react"
-import { useEffect } from "react"
+import React, { useRef, MouseEvent, PropsWithChildren, FC } from "react"
 import ZoomContainer from "../../Containers/ZoomContainer"
-import NodeRenderer, { NodeTypeProps } from "../../Renderers/NodeRenderer"
+import NodeRenderer from "../../Renderers/NodeRenderer"
+import StoreUpdater from "../StoreUpdater"
 import { EdgeRenderer } from "../../Renderers/EdgeRenderer"
 import { InteractionRenderer } from "../../Renderers/InteractionRenderer"
 import { calculateScaledMousePosition } from "./utils"
 import { useStore } from "../../Hooks/useStore"
-import { IEdge, IGraphState, INode } from "../../Types"
-import { useStoreApi } from "../../Hooks/useStoreApi"
+import { IGraphProps, IGraphState } from "../../Types"
 
-interface GraphProps {
-  children: React.ReactNode;
-  nodes: INode[];
-  nodeTypes: { [key: string]: ComponentType<NodeTypeProps> };
-  edges: IEdge[];
-}
 
 const selector = (s: IGraphState) => ({
   graphNodes: s.nodes,
@@ -32,69 +25,34 @@ const selector = (s: IGraphState) => ({
  * @param param0 
  * @returns 
  */
-export const Graph = ({children, nodes, nodeTypes, edges}: GraphProps) => {
+export const Graph: FC<PropsWithChildren<IGraphProps>> = ({children,
+  nodes,
+  edges,
+  nodeTypes,
+  enableDraggableNodes,
+}) => {
   const flowRef = useRef<HTMLDivElement>(null)
-  const store = useStoreApi()
-  const nodesRef = useRef(useStoreApi().getState().nodes)
-  const edgesRef = useRef(useStoreApi().getState().edges)
-  const {transform, handleInteraction, setHandleInteraction, resetHandleInteraction, setCustomNodeTypes, addNode, addEdge} = useStore(selector)
+  const store = useStore(selector)
   
-  /**
-   * Enables the nodesRef to subscribe to state.nodes
-   */
-  useEffect(() => store.subscribe(
-    state => (nodesRef.current = state.nodes)
-  ), [store])
-
-  /**
-   * When nodeData is modified, publish changes to the NodeStore
-   */
-  useEffect(() => {
-    nodes.forEach(node => {
-      if(!nodesRef.current.some(n => n.id === node.id)){
-        addNode(node)
-      }
-    })
-  }, [addNode, nodes, nodesRef])
-
-  /**
-   * Publish nodeTypes to the store
-   */
-  useEffect(() => {
-    setCustomNodeTypes(nodeTypes);
-    }, [setCustomNodeTypes, nodeTypes]);
-
-  /**
-   * Subscribe to edge store to enable edgeRef to follow edgeStore.
-   */
-  useEffect(() => store.subscribe(
-    state => (edgesRef.current = state.edges)
-  ), [store])
-
-  /**
-   * On EdgeData change, add nex edges to EdgeStore
-   */
-  useEffect(() => {
-    edges.forEach(edge => {
-      if(!edgesRef.current.find(e => e.id === edge.id)){
-        addEdge(edge)
-      }
-    })
-  })
-
   const handleMouseMove = (e: MouseEvent) => {
-    if(handleInteraction !== undefined && flowRef.current !== null){
-      setHandleInteraction({...handleInteraction, mousePosition: calculateScaledMousePosition(e, flowRef.current, transform)})
+    if(store.handleInteraction !== undefined && flowRef.current !== null){
+      store.setHandleInteraction({...store.handleInteraction, mousePosition: calculateScaledMousePosition(e, flowRef.current, store.transform)})
     }
   }
 
   return (
     <div
       ref={flowRef}
-      onMouseUp={() => resetHandleInteraction()}
+      onMouseUp={() => store.resetHandleInteraction()}
       onMouseMove={(e: MouseEvent) => handleMouseMove(e)}
       style={{width: '100%', height: '100%', overflow: "hidden", position: "relative"}}
     >
+      <StoreUpdater
+        nodes={nodes}
+        edges={edges}
+        nodeTypes={nodeTypes}
+        enableDraggableNodes={enableDraggableNodes}
+      />
       <ZoomContainer>
         <NodeRenderer />
         <EdgeRenderer />
