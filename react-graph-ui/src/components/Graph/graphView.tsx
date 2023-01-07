4787aa93df74c622b719405488cd17160ab87a3d
Matt Kirby <MattJKirby@outlook.com>
2023-01-07 13:39:05 +0000

    Added a new graph view component to remove all view functionality away from graph component

---
+++ react-graph-ui/src/components/Graph/graphView.tsx
@@ -0,0 +1,45 @@
+import { FC, PropsWithChildren, useRef, MouseEvent } from "react";
+import ZoomContainer from "../../Containers/ZoomContainer";
+import { useStore } from "../../Hooks/useStore";
+import EdgeRenderer from "../../Renderers/EdgeRenderer";
+import { InteractionRenderer } from "../../Renderers/InteractionRenderer";
+import NodeRenderer from "../../Renderers/NodeRenderer";
+import { IGraphState } from "../../Types";
+import { calculateScaledMousePosition } from "./utils";
+
+const selector = (s: IGraphState) => ({
+  graphNodes: s.nodes,
+  transform: s.graphTransform,
+  handleInteraction: s.handleInteraction,
+  setHandleInteraction: s.setHandleInteraction,
+  resetHandleInteraction: s.resetHandleInteraction
+});
+
+const GraphView: FC<PropsWithChildren> = ({children}) => {
+  const flowRef = useRef<HTMLDivElement>(null)
+  const store = useStore(selector)
+  
+  const handleMouseMove = (e: MouseEvent) => {
+    if(store.handleInteraction !== undefined && flowRef.current !== null){
+      store.setHandleInteraction({...store.handleInteraction, mousePosition: calculateScaledMousePosition(e, flowRef.current, store.transform)})
+    }
+  }
+
+  return (
+    <div
+      ref={flowRef}
+      onMouseUp={() => store.resetHandleInteraction()}
+      onMouseMove={(e: MouseEvent) => handleMouseMove(e)}
+      style={{width: "100%", height: "100%", overflow: "hidden", position: "relative"}}
+      >
+        <ZoomContainer>
+          <NodeRenderer />
+          <EdgeRenderer />
+          <InteractionRenderer />
+        </ZoomContainer>
+        {children}
+    </div>
+  )
+}
+
+export default GraphView;
