cd0fd3549abfd8fed9fb22fc6258da99d047af7b
Matt Kirby <MattJKirby@outlook.com>
2023-01-07 13:41:23 +0000

    Added GraphWrapper and GraphView components to graph entry point

--- react-graph-ui/src/components/Graph/index.tsx
+++ react-graph-ui/src/components/Graph/index.tsx
@@ -1,21 +1,8 @@
-import React, { useRef, MouseEvent, PropsWithChildren, FC } from "react"
-import ZoomContainer from "../../Containers/ZoomContainer"
-import NodeRenderer from "../../Renderers/NodeRenderer"
+import React, { PropsWithChildren, FC } from "react"
 import StoreUpdater from "../StoreUpdater"
-import { EdgeRenderer } from "../../Renderers/EdgeRenderer"
-import { InteractionRenderer } from "../../Renderers/InteractionRenderer"
-import { calculateScaledMousePosition } from "./utils"
-import { useStore } from "../../Hooks/useStore"
-import { IGraphProps, IGraphState } from "../../Types"
-
-
-const selector = (s: IGraphState) => ({
-  graphNodes: s.nodes,
-  transform: s.graphTransform,
-  handleInteraction: s.handleInteraction,
-  setHandleInteraction: s.setHandleInteraction,
-  resetHandleInteraction: s.resetHandleInteraction
-});
+import { IGraphProps } from "../../Types"
+import GraphWrapper from "./graphWrapper"
+import GraphView from "./graphView"
 
 /**
  * Graph entrypoint component
@@ -29,35 +16,19 @@ export const Graph: FC<PropsWithChildren<IGraphProps>> = ({
   nodeTypes,
   enableDraggableNodes,
 }) => {
-  const flowRef = useRef<HTMLDivElement>(null)
-  const store = useStore(selector)
-  
-  const handleMouseMove = (e: MouseEvent) => {
-    if(store.handleInteraction !== undefined && flowRef.current !== null){
-      store.setHandleInteraction({...store.handleInteraction, mousePosition: calculateScaledMousePosition(e, flowRef.current, store.transform)})
-    }
-  }
 
   return (
-    <div
-      ref={flowRef}
-      onMouseUp={() => store.resetHandleInteraction()}
-      onMouseMove={(e: MouseEvent) => handleMouseMove(e)}
-      style={{width: '100%', height: '100%', overflow: "hidden", position: "relative"}}
-    >
-      <StoreUpdater
-        nodes={nodes}
-        edges={edges}
-        nodeTypes={nodeTypes}
-        enableDraggableNodes={enableDraggableNodes}
-      />
-      <ZoomContainer>
-        <NodeRenderer />
-        <EdgeRenderer />
-        <InteractionRenderer />
-      </ZoomContainer>
-      {children}
-    </div> 
+    <GraphWrapper>
+      <GraphView>
+        <StoreUpdater
+          nodes={nodes}
+          edges={edges}
+          nodeTypes={nodeTypes}
+          enableDraggableNodes={enableDraggableNodes}
+        />
+        {children}
+        </GraphView>
+    </GraphWrapper>
   )
 }
 
