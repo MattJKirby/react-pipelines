1ab8b356e90fa0224128ac596327b0699877c6e4
Matt Kirby <MattJKirby@outlook.com>
2023-01-16 17:54:36 +0000

    Added gratGridLines method

--- react-graph-ui/src/components/Canvas/utils.tsx
+++ react-graph-ui/src/components/Canvas/utils.tsx
@@ -1,6 +1,6 @@
 
 /**
- * Method for generating a grid pattern of specific dimensions
+ * Method for generating a dotted grid pattern of specific dimensions
  * @param size 
  * @param fill 
  * @returns 
@@ -8,3 +8,13 @@
 export const createGridDotsPath = (size: number, fill: string): React.ReactElement => {
   return <circle cx={size} cy={size} r={size} fill={fill} />;
 };
+
+/**
+ * Method for creating grid pattern
+ * @param size 
+ * @param stroke 
+ * @returns 
+ */
+export const createGridLinesPath = (size: number, stroke: string): React.ReactElement => {
+  return <path d={`M ${size} 0 L 0 0 0 ${size}`} fill="none" stroke={stroke} strokeWidth="1"/>
+}
