46142b2bbdda13e7ed1723cd376f48a34a568f54
Matt Kirby <MattJKirby@outlook.com>
2023-01-17 20:48:28 +0000

    Added default node structure

--- react-graph-ui/src/components/Node/DefaultNode.tsx
+++ react-graph-ui/src/components/Node/DefaultNode.tsx
@@ -1,14 +1,21 @@
 import React, { memo } from "react"
 import { INodeProps } from "../../Types";
-
 import { Handle } from "../Handle";
+import styles from '../../Styles/Node/DefaultNode.module.css'
 
 const DefaultNode = ({data}: INodeProps) => {
 
   return (
     <>
-      {data?.label}
-      <Handle id="source"/>
+      <div className={styles.RP_DefaultNode__Container}>
+        <div className={`${styles.RP_DefaultNode__Content} ${styles.RP_DefaultNode__LabelHeader}`}>
+          {data?.label}
+        </div>
+
+        <div className={`${styles.RP_DefaultNode__Content}`}>
+          <Handle id="source"/>
+        </div>
+      </div>
     </>
   )
 }
