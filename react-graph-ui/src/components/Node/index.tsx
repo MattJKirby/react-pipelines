44a15be772e6b8c1e94bf68b41372f9cb00818c5
Matt Kirby <MattJKirby@outlook.com>
2023-01-24 01:25:12 +0000

    Node wrapper now captures mouseDown

--- react-graph-ui/src/components/Node/index.tsx
+++ react-graph-ui/src/components/Node/index.tsx
@@ -3,7 +3,8 @@ import React, { memo, useEffect, useState } from "react"
 import NodeDataContext from "../../Contexts/NodeDataContext"
 import { IGraphState, INode, IXYPosition } from "../../Types";
 import { useStore } from "../../Hooks/useStore";
-
+import { nodeClickHandler } from "./utils";
+import { useStoreApi } from "../../Hooks/useStoreApi";
 
 export interface NodeContainerProps {
   children: React.ReactNode;
@@ -13,16 +14,19 @@ export interface NodeContainerProps {
 const selector = (s: IGraphState) => ({
   nodeDragInteraction: s.nodeDragInteraction,
   enableDraggableNodes: s.enableDraggableNodes,
+
   updateNodePosition: s.updateNodePosition,
   setNodeDragInteraction: s.setNodeDragInteraction,
   resetNodeDragInteraction: s.resetNodeDragInteraction,
+  addSelectedNode: s.addSelectedNode,
 });
 
 const Node = ({children, node}: NodeContainerProps) => {
-  const state = useStore(selector)
-  const [isDragging, setIsDragging] = useState(false)
-  const [position, setPosition] = useState(node.position)
-  
+  const state = useStore(selector);
+  const store = useStoreApi();
+  const [isDragging, setIsDragging] = useState(false);
+  const [position, setPosition] = useState(node.position);
+
   const handlePositionUpdate = (position: IXYPosition) => {
     if(state.enableDraggableNodes && position !== node?.position){
       setPosition(position)
@@ -35,12 +39,15 @@ const Node = ({children, node}: NodeContainerProps) => {
     isDragging ? state.setNodeDragInteraction(node.id) : state.resetNodeDragInteraction();
    }
   }
-  ,[isDragging, node.id, state])
+  ,[isDragging, node.id, state]);
   
   return (
     <NodeDataContext.Provider value={node}>
       <DragContainer initPosition={node.position} onPositionUpdate={handlePositionUpdate} onDrag={setIsDragging}>
-        <div style={{left: `${position.x}px`, top: `${position.y}px`, position: 'fixed', userSelect: "none"}}>
+        <div 
+          style={{left: `${position.x}px`, top: `${position.y}px`, position: 'fixed', userSelect: "none"}}
+          onMouseDownCapture={() => nodeClickHandler({id: node.id, store: store})}
+          >
           {children}
         </div>
       </DragContainer>
