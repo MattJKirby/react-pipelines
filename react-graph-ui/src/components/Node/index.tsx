9995e87139fe5e5abbe0a022519402b9cb69622a
Matt Kirby <MattJKirby@outlook.com>
2023-01-26 23:00:02 +0000

    Removed old drag container code

--- react-graph-ui/src/components/Node/index.tsx
+++ react-graph-ui/src/components/Node/index.tsx
@@ -1,8 +1,6 @@
-import DragContainer from "../../Containers/DragContainer"
-import React, { memo, useEffect, useRef, useState } from "react"
+import React, { memo, useRef } from "react"
 import NodeDataContext from "../../Contexts/NodeDataContext"
-import { IGraphState, INode, IXYPosition } from "../../Types";
-import { useStore } from "../../Hooks/useStore";
+import { INode } from "../../Types";
 import { nodeClickHandler } from "./utils";
 import { useStoreApi } from "../../Hooks/useStoreApi";
 import useDrag from "../../Hooks/useDrag";
@@ -12,50 +10,20 @@ export interface NodeContainerProps {
   node: INode;
 }
 
-const selector = (s: IGraphState) => ({
-  nodeDragInteraction: s.nodeDragInteraction,
-  enableDraggableNodes: s.enableDraggableNodes,
-
-  updateNodePosition: s.updateNodePosition,
-  setNodeDragInteraction: s.setNodeDragInteraction,
-  resetNodeDragInteraction: s.resetNodeDragInteraction,
-  addSelectedNode: s.addSelectedNode,
-});
-
 const Node = ({children, node}: NodeContainerProps) => {
-  const state = useStore(selector);
   const store = useStoreApi();
   const nodeRef = useRef<HTMLDivElement>(null);
-  const [isDragging, setIsDragging] = useState(false);
-  const [position, setPosition] = useState(node.position);
-
-  const handlePositionUpdate = (position: IXYPosition) => {
-    if(state.enableDraggableNodes && position !== node?.position){
-      setPosition(position)
-      state.updateNodePosition(node.id,position)
-    }
-  }
-
-  useEffect(() => {
-   if(state.nodeDragInteraction !== undefined){
-    isDragging ? state.setNodeDragInteraction(node.id) : state.resetNodeDragInteraction();
-   }
-  }
-  ,[isDragging, node.id, state]);
-
-  useDrag({nodeId: node.id, nodeRef: nodeRef, position: node.position})
+  const dragging = useDrag({store: store, nodeId: node.id, nodeRef: nodeRef, position: node.position})
   
   return (
     <NodeDataContext.Provider value={node}>
-      {/* <DragContainer initPosition={node.position} onPositionUpdate={handlePositionUpdate} onDrag={setIsDragging}> */}
-        <div
-          ref={nodeRef}
-          style={{left: `${node.position.x}px`, top: `${node.position.y}px`, position: 'fixed', userSelect: "none"}}
-          onMouseDownCapture={() => nodeClickHandler({id: node.id, store: store})}
-          >
-          {children}
-        </div>
-      {/* </DragContainer> */}
+      <div
+        ref={nodeRef}
+        style={{left: `${node.position.x}px`, top: `${node.position.y}px`, position: 'fixed', userSelect: "none"}}
+        onMouseDownCapture={() => nodeClickHandler({id: node.id, store: store})}
+        >
+        {children}
+      </div>
     </NodeDataContext.Provider>
   )
 }
