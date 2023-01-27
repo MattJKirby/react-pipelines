b8b5c5dd82344c6bc883ba870e50bbcf0cdf910f
Matt Kirby <MattJKirby@outlook.com>
2023-01-27 01:02:44 +0000

    NodeWrapper context now uses partial node

--- react-graph-ui/src/Contexts/NodeDataContext.tsx
+++ react-graph-ui/src/Contexts/NodeDataContext.tsx
@@ -1,9 +1,9 @@
 import { createContext, useContext } from 'react';
 import { INode } from '../Types';
 
-export const NodeDataContext = createContext<INode | undefined>(undefined);
+export const NodeDataContext = createContext<Partial<INode> | undefined>(undefined);
 
-export const useNodeContext = (): INode | undefined => {
+export const useNodeContext = (): Partial<INode> | undefined => {
   const nodeData = useContext(NodeDataContext);
   return nodeData;
 };
--- react-graph-ui/src/components/Node/index.tsx
+++ react-graph-ui/src/components/Node/index.tsx
@@ -1,26 +1,43 @@
 import React, { memo, useRef } from "react"
 import NodeDataContext from "../../Contexts/NodeDataContext"
-import { INode } from "../../Types";
-import { nodeClickHandler } from "./utils";
+import { IXYPosition } from "../../Types";
+import { nodeSelectHandler } from "./utils";
 import { useStoreApi } from "../../Hooks/useStoreApi";
 import useDrag from "../../Hooks/useDrag";
 
 export interface NodeContainerProps {
   children: React.ReactNode;
-  node: INode;
+  id: string;
+  type: string;
+  position: IXYPosition;
+  selected: boolean;
+  enableSelect: boolean;
+  enableDrag: boolean;
+  selectOnDrag: boolean;
 }
 
-const Node = ({children, node}: NodeContainerProps) => {
+const Node = ({
+  children, 
+  id,
+  position,
+  enableSelect,
+  enableDrag,
+  selectOnDrag
+  
+}: NodeContainerProps) => {
   const store = useStoreApi();
   const nodeRef = useRef<HTMLDivElement>(null);
-  const dragging = useDrag({store: store, nodeId: node.id, nodeRef: nodeRef, position: node.position})
+  const dragging = useDrag({store: store, nodeId: id, nodeRef: nodeRef, position: position, disabled: !enableDrag, selectOnDrag: selectOnDrag});
   
   return (
-    <NodeDataContext.Provider value={node}>
+    <NodeDataContext.Provider value={{
+      id: id, 
+      position: position
+    }}>
       <div
         ref={nodeRef}
-        style={{transform: `translate(${node.position.x}px, ${node.position.y}px)`, position: 'fixed', userSelect: "none"}}
-        onMouseDownCapture={() => nodeClickHandler({id: node.id, store: store})}
+        style={{transform: `translate(${position.x}px, ${position.y}px)`, position: 'fixed', userSelect: "none"}}
+        onClickCapture={() => nodeSelectHandler({id: id, store: store, disabled: !enableSelect})}
         >
         {children}
       </div>
