369e9b69e75161b0de64941b4b01ba8e0ffb4d12
Matt Kirby <MattJKirby@outlook.com>
2023-01-28 22:38:24 +0000

    Optimise NodeWrapper on drag

--- react-graph-ui/src/components/Node/index.tsx
+++ react-graph-ui/src/components/Node/index.tsx
@@ -1,20 +1,9 @@
 import React, { memo, useRef } from "react"
 import NodeDataContext from "../../Contexts/NodeDataContext"
-import { IXYPosition } from "../../Types";
+import { NodeContainerProps } from "../../Types";
 import { nodeSelectHandler } from "./utils";
 import { useStoreApi } from "../../Hooks/useStoreApi";
 import useDrag from "../../Hooks/useDrag";
-
-export interface NodeContainerProps {
-  children: React.ReactNode;
-  id: string;
-  type: string;
-  position: IXYPosition;
-  selected: boolean;
-  enableSelect: boolean;
-  enableDrag: boolean;
-  selectOnDrag: boolean;
-}
 
 const Node = ({
   children, 
@@ -28,7 +17,7 @@ const Node = ({
   const store = useStoreApi();
   const nodeRef = useRef<HTMLDivElement>(null);
   const dragging = useDrag({store: store, nodeId: id, nodeRef: nodeRef, position: position, disabled: !enableDrag, selectOnDrag: selectOnDrag});
-  
+
   return (
     <NodeDataContext.Provider value={{
       id: id, 
@@ -36,7 +25,7 @@ const Node = ({
     }}>
       <div
         ref={nodeRef}
-        style={{transform: `translate(${position.x}px, ${position.y}px)`, position: 'fixed', userSelect: "none"}}
+        style={{transform: `translate(${dragging.x}px, ${dragging.y}px)`, position: 'fixed', userSelect: "none"}}
         onClickCapture={() => nodeSelectHandler({id: id, store: store, disabled: !enableSelect})}
         >
         {children}
