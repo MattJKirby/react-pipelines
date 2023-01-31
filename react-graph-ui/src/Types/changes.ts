8a12d0fd704a06ef6e5f3c947c0659b8285ca459
Matt Kirby <MattJKirby@outlook.com>
2023-01-31 17:23:14 +0000

    Added new change and changeData types

---
+++ react-graph-ui/src/Types/changes.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { IXYPosition } from "./generic"
+import { INode } from "./node";
+
+export type NodeChange<Type, ChangeData> = {
+  type: Type;
+  changeData: ChangeData;
+}
+
+export type ChangeTypes =
+  | NodePositionChange 
+  | NodeSelectionChange
+  | NodeAddChange;
+
+export type NodePositionChange = NodeChange<'position', NodePositionChangeData>;
+export type NodeSelectionChange = NodeChange<'select', NodeSelectionChangeData>;
+export type NodeAddChange = NodeChange<'add', NodeAddChangeData>;
+
+export type NodePositionChangeData = {
+  id: string;
+  position: IXYPosition;
+  dragging: boolean;
+}
+
+export type NodeSelectionChangeData = {
+  id: string;
+  selected: boolean;
+}
+
+export type NodeAddChangeData<NodeData = any> = {
+  node: INode<NodeData>;
+}
+
+
