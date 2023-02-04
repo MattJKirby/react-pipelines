67b48bc00231c88efa86b9519167caf13501dfef
Matt Kirby <MattJKirby@outlook.com>
2023-02-04 01:55:38 +0000

    Added useEdgeStore

--- react-graph-ui/src/Hooks/useStoreItemState.ts
+++ react-graph-ui/src/Hooks/useStoreItemState.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { SetStateAction, Dispatch, useCallback } from 'react';
 import { useState } from "react"
-import { applyNodeChanges } from '../Changes';
+import { applyEdgeChanges, applyNodeChanges } from '../Changes';
 import { IEdge, INode } from "../Types"
-import { NodeChangeTypes } from '../Types/changes';
+import { EdgeChangeTypes, NodeChangeTypes } from '../Types/changes';
 
 type ApplyChanges<ChangeType, ItemType> = (changed: ChangeType[], items: ItemType[]) => ItemType[];
 type OnChange<ChangeType> = (changes: ChangeType[]) => void;
 
 function createUseStoreItemsState(applyChanges: ApplyChanges<NodeChangeTypes, INode>): <NodeData = any>(initialItems: INode<NodeData>[]) => [INode<NodeData>[], Dispatch<SetStateAction<INode<NodeData>[]>>, OnChange<NodeChangeTypes>]
-// function createUseStoreItemsState(applyChanges: ApplyChanges<IEdge>): (initialItems: IEdge[]) => [IEdge[], Dispatch<SetStateAction<IEdge[]>>, OnChange<ChangeTypes>]
+function createUseStoreItemsState(applyChanges: ApplyChanges<EdgeChangeTypes, IEdge>): (initialItems: IEdge[]) => [IEdge[], Dispatch<SetStateAction<IEdge[]>>, OnChange<EdgeChangeTypes>]
 
 function createUseStoreItemsState(applyChanges: ApplyChanges<any, any>): (initialItems: any[]) => [any[], Dispatch<SetStateAction<any[]>>, OnChange<any>] {
   return (initialItems: any[]) => {
@@ -25,4 +25,4 @@ function createUseStoreItemsState(applyChanges: ApplyChanges<any, any>): (initia
 
 
 export const useNodesStore = createUseStoreItemsState(applyNodeChanges);
-// export const useEdgeStore = createUseStoreItemsState(applyEdgeChanges);
+export const useEdgeStore = createUseStoreItemsState(applyEdgeChanges);
