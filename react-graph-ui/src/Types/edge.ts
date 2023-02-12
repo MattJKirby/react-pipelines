6a6b05aaf77b3d42985837dced63e8387d468aed
Matt Kirby <MattJKirby@outlook.com>
2023-02-12 00:50:22 +0000

    Edges now have selected props

--- react-graph-ui/src/Types/edge.ts
+++ react-graph-ui/src/Types/edge.ts
@@ -7,13 +7,16 @@ export interface IEdge {
   targetNodeId: string;
   targetNodeInput: string;
   type: string;
+  selected?: boolean;
 }
 
 export interface IEdgeProps {
-  children: React.ReactNode
-  edge: IEdge
-  source: IHandle
-  target: IHandle
+  children: React.ReactNode;
+  edge: IEdge;
+  source: IHandle;
+  target: IHandle;
+  selected: boolean;
+  enableSelect: boolean;
 }
 
 export type EdgeInternals = Map<string, IEdge>;
