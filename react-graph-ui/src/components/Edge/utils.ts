5a10d9a24754ab940dfb585d234cd41a3ad305d0
Matt Kirby <MattJKirby@outlook.com>
2023-02-13 00:01:35 +0000

    Fixed edgeSelection handler

--- react-graph-ui/src/components/Edge/utils.ts
+++ react-graph-ui/src/components/Edge/utils.ts
@@ -4,7 +4,8 @@ import { CalculateStraightPath } from "./pathTypes";
 export const edgeSelectHandler = ({
   id,
   store,
-  disabled
+  disabled = false,
+  unselect = false
 }: SelectHandlerProps) => {
   const { updateSelectedEdges, resetSelectedEdges, edgeInternals } = store.getState();
   const edge = edgeInternals.get(id);
@@ -13,6 +14,8 @@ export const edgeSelectHandler = ({
     if(!edge?.selected){
       resetSelectedEdges();
       updateSelectedEdges([{id, selected: true}])
+    } else if (unselect){
+      updateSelectedEdges([{id, selected: false}])
     }
   }
 }
