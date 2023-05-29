aa0aafafe7ee58ca48cc7e5d3ff9739695196a6c
Matt Kirby <MattJKirby@outlook.com>
2023-05-30 00:14:02 +0100

    Replaced graph transform useEffect with custom hook

--- react-graph-ui/src/Containers/ZoomContainer.tsx
+++ react-graph-ui/src/Containers/ZoomContainer.tsx
@@ -1,10 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import {useEffect, useRef } from "react";
-import * as d3 from "d3-zoom";
-import {select} from 'd3-selection'
+import { useRef } from "react";
 import React from "react";
-import { useStore } from "../Hooks/useStore";
-
+import { useStoreApi } from "../Hooks/useStoreApi";
+import useGraphTransform from "../Hooks/useGraphTransform";
 
 interface FlowZoomProps {
   children?: React.ReactNode
@@ -12,28 +10,12 @@ interface FlowZoomProps {
 
 export const ZoomContainer = ({children}: FlowZoomProps) => {
   const zoomContainer = useRef<HTMLDivElement>(null);
-  const zoom = useRef<HTMLDivElement>(null);
-  const transform= useStore(s => s.graphTransform)
-  const setTransform = useStore(s => s.setGraphTransform)
-  const zoomFilter = (e: any) => e.target.closest('.flow-ui-noZoom') === null;
-
-  useEffect(() => {
-    if(zoomContainer.current){
-      const zoom = d3.zoom().on("zoom", (event) => {
-        const { x, y, k } = event.transform;
-        setTransform({scale: k, translateX: x, translateY: y})
-
-        event.sourceEvent.preventDefault()
-        event.sourceEvent.stopPropagation();
-      }).scaleExtent([0.1, 2]).filter(event => zoomFilter(event));
-      select(zoomContainer.current as Element).call(zoom);
-    }
-    
-  }, [setTransform])
+  const store = useStoreApi();
+  const transform = useGraphTransform({store, transformerRef: zoomContainer})
 
   return (
     <div className="RP_ZoomViewport__Container" ref={zoomContainer} style={{ width: '100%', height: '100%', zIndex: -1, backgroundColor: 'transparent'}}>
-      <div className="RP_ZoomViewport" ref={zoom} style={{ height: "100%", left: 0, top: 0, width: "100%", transform: `translate(${transform.translateX}px, ${transform.translateY}px) scale(${transform.scale})`, transformOrigin: '0 0', position: 'relative'}}>
+      <div className="RP_ZoomViewport" style={{ height: "100%", left: 0, top: 0, width: "100%", transform: `translate(${transform.translateX}px, ${transform.translateY}px) scale(${transform.scale})`, transformOrigin: '0 0', position: 'relative'}}>
         {children}
       </div>
     </div>
---
+++ react-graph-ui/src/Hooks/useGraphTransform.ts
@@ -0,0 +1,40 @@
+import { RefObject, useEffect } from "react";
+import { INodeStore } from "../Types";
+import * as d3 from "d3-zoom";
+import {select} from 'd3-selection'
+
+type useGraphTransformProps = {
+  store: INodeStore;
+  transformerRef: RefObject<HTMLDivElement>;
+}
+
+/**
+ * Hook for enabling control and mutation of the graph transform.
+ * @param param0 
+ * @returns the new graph transform
+ */
+const useGraphTransform = ({
+  store,
+  transformerRef
+}: useGraphTransformProps) => {
+  const { graphTransform, setGraphTransform } = store.getState();
+  const zoomFilter = (e: any) => e.target.closest('.flow-ui-noZoom') === null;
+
+  useEffect(() => {
+    if(transformerRef.current){
+      const zoom = d3.zoom().on("zoom", (event) => {
+        const { x, y, k } = event.transform;
+        setGraphTransform({scale: k, translateX: x, translateY: y})
+ 
+        event.sourceEvent.preventDefault()
+        event.sourceEvent.stopPropagation();
+      }).scaleExtent([0.1, 2]).filter(event => zoomFilter(event));
+      select(transformerRef.current as Element).call(zoom);
+    }
+    
+  }, [graphTransform, setGraphTransform, transformerRef])
+
+  return graphTransform
+}
+
+export default useGraphTransform;
