cb5b61a7610f2ce0743c8a75124716c7d7aa8bf2
Matt Kirby <MattJKirby@outlook.com>
2023-06-23 22:11:13 +0200

    Moved viewport calculations out of store

--- react-graph-ui/src/components/Graph/utils.ts
+++ react-graph-ui/src/components/Graph/utils.ts
@@ -1,5 +1,5 @@
 import { MouseEvent } from 'react'
-import { ITransform, IXYPosition } from "../../Types";
+import { Dimension, ITransform, IXYPosition, Rect } from "../../Types";
 
 /**
  * Method for calculating scaled mouse position relative to a given element
@@ -24,4 +24,41 @@ export const TransformPosition = (position: IXYPosition, transform: ITransform) 
   const transformedY = (position.y - transform.translateY) / transform.scale;
 
   return {x: transformedX, y: transformedY}
+}
+
+/**
+ * Calculate the graph viewport rect from a transform and graph dimensions
+ * @param transform 
+ * @param graphDimensions 
+ * @returns 
+ */
+export const CalculateGraphViewportRect = (transform: ITransform, graphDimensions: Dimension): Rect => {
+  const { translateX, translateY, scale } = transform;
+  return {
+    x: -translateX / scale,
+    y: -translateY / scale,
+    width: graphDimensions.width / scale,
+    height: graphDimensions.height / scale
+  }
+}
+
+/**
+ * Calculate the required graph transform for a given viewport rect
+ * @param viewportRect 
+ * @param graphDimensions 
+ * @returns 
+ */
+export const CalculateGraphTransformForViewport = (viewportRect: Rect, graphDimensions: Dimension): ITransform => {
+  const scaleX = graphDimensions.width / viewportRect.width;
+  const scaleY = graphDimensions.height / viewportRect.height;
+  const scale = Math.min(scaleX, scaleY);
+
+  const translateX = -viewportRect.x * scale + (graphDimensions.width - viewportRect.width * scale) / 2;
+  const translateY = -viewportRect.y * scale + (graphDimensions.height - viewportRect.height * scale) / 2;
+
+  return {
+    translateX,
+    translateY,
+    scale,
+  };
 }
