b334b59046cc91bcec69138e1c0c9d6850e92aa0
Matt Kirby <MattJKirby@outlook.com>
2023-06-28 19:15:51 +0200

    Added basic controls styles

---
+++ react-graph-ui/src/Styles/Controls/index.ts
@@ -0,0 +1,18 @@
+import { CSSProperties } from "react";
+
+export const ControlButtonStyles: CSSProperties = {
+  all: "unset",
+  background: "none",
+  border: "1px solid black",
+  width: "24px",
+  height: "24px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  margin: "1px"
+};
+
+export const ControlNameStyles: CSSProperties = {
+  margin: 0,
+  fontSize: "x-small"
+}
