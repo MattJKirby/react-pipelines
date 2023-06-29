501986fd91036e9873dff09d1c41fd60c27f9dcb
Matt Kirby <MattJKirby@outlook.com>
2023-06-30 01:08:05 +0200

    bezier path calculations

--- react-graph-ui/src/components/Edge/pathTypes.ts
+++ react-graph-ui/src/components/Edge/pathTypes.ts
@@ -1,5 +1,49 @@
-import { IHandle } from "../../Types"
+import { IXYPosition, Position } from "../../Types"
 
-export const CalculateStraightPath = (source: IHandle, target: IHandle): string => {
-  return `M${source.position.x} ${source.position.y} L ${target.position.x} ${target.position.y}`
+/**
+ * Straight vector path
+ * @param source 
+ * @param target 
+ * @returns 
+ */
+export const CalculateStraightPath = (source: IXYPosition, target: IXYPosition): string => {
+  return `M${source.x} ${source.y} L ${target.x} ${target.y}`;
+};
+
+/**
+ * Bezier vector path
+ * @param source 
+ * @param target 
+ * @returns 
+ */
+export const CalculateBezierPath = (source: IXYPosition, target: IXYPosition): string => {
+  const sourcePosition = "right";
+  const targetPosition = "left";
+  const curvature = 0.5;
+  const sourceControl = calculateBezierControl(sourcePosition, source, target, curvature);
+  const targetControl = calculateBezierControl(targetPosition, target, source, curvature);
+  
+  return `M${source.x},${source.y} C${sourceControl[0]},${sourceControl[1]} ${targetControl[0]},${targetControl[1]} ${target.x},${target.y}`;
 }
+
+const calculateControlOffset = (distance: number, curvature: number): number => {
+  if (distance >= 0) {
+    return 0.5 * distance;
+  }
+
+  return curvature * 25 * Math.sqrt(-distance);
+}
+
+const calculateBezierControl = (pos: Position, source: IXYPosition, target: IXYPosition, c: number): [x: number, y: number] => {
+  switch (pos) {
+    case "left":
+      return [source.x - calculateControlOffset(source.x - target.x, c), source.y];
+    case "right":
+      return [source.x + calculateControlOffset(target.x - source.x, c), source.y];
+    case "top":
+      return [source.x, source.y - calculateControlOffset(source.y - target.y, c)];
+    case "bottom":
+      return [source.x, source.y + calculateControlOffset(target.y - source.y, c)];
+  }
+}
+
