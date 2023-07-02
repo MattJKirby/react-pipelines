7f0c3b8a748d1982b9b9b22ebec0ae95fdb474f4
Matt Kirby <MattJKirby@outlook.com>
2023-07-03 00:20:33 +0200

    GetNodeData utility method for edges checks validity of data

--- react-graph-ui/src/components/Edge/utils.ts
+++ react-graph-ui/src/components/Edge/utils.ts
@@ -1,4 +1,5 @@
-import { PathTypeMap, SelectHandlerProps } from "../../Types"
+import { IHandle, INode, INodeHandles, IXYPosition, PathTypeMap, SelectHandlerProps } from "../../Types"
+import { combineMaps, internalsSymbol } from "../../Utils";
 import { CalculateBezierPath, CalculateStraightPath } from "./pathTypes";
 
 export const edgeSelectHandler = ({
@@ -24,4 +25,41 @@ export const edgePathTypeMap: PathTypeMap = new Map([
   ['straight', CalculateStraightPath],
   ['bezier', CalculateBezierPath]
 ]);
+
+/**
+ * Extracts the relevant handle data from nodes
+ * @param node 
+ * @returns 
+ */
+export const getNodeData = (node?: INode):[INodeHandles | null, IXYPosition, boolean] => {
+  const handles = node?.[internalsSymbol]?.handles || null;
+  
+  const isValid = 
+    handles !== null &&
+    node?.position !== undefined
+
+  return [
+    handles, 
+    {
+      x: node?.position.x || 0,
+      y: node?.position.y || 0
+    }, 
+    isValid];
+} 
+
+/**
+ * Calculate the edge start and end position
+ * @param sourceNode 
+ * @param sourceHandle 
+ * @param targetNode 
+ * @param targetHandle 
+ */
+export const getEdgePositions = (sourceNodePosition: IXYPosition, sourceHandle: IHandle, targetNodePosition: IXYPosition, targetHandle: IHandle) => {
+  return {
+    sourceX: sourceNodePosition.x + sourceHandle.x,
+    sourceY: sourceNodePosition.y + sourceHandle.y,
+    targetX: targetNodePosition.x + targetHandle.x,
+    targetY: targetNodePosition.y + targetHandle.y
+  }
+}
 
