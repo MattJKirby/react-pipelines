afbb68e9e438a48d4376b66179eac8c5a6a03c7c
Matt Kirby <MattJKirby@outlook.com>
2023-08-22 00:55:21 +0200

    Added default exports to index file

---
+++ react-graph-ui/src/index.ts
@@ -0,0 +1,19 @@
+// Components
+export { default as GraphCanvas } from './Components/Canvas'
+export { default as Controls } from './Components/Controls'
+export { default as DefaultEdge } from './Components/Edge/DefaultEdge'
+export { default as Graph } from './Components/Graph'
+export { default as GraphProvider } from './Components/GraphProvider'
+export { default as Handle } from './Components/Handle'
+export { default as MiniMap } from './Components/MiniMap'
+export { default as DefaultNode } from './Components/Node/DefaultNode'
+export { default as TestNode } from './Components/Node/TestNode'
+export { default as Panel } from './Components/Panel'
+
+// Changes
+export { applyNodeChanges, applyEdgeChanges } from './Changes'
+
+// Hooks
+export { default as useDynamicDimensions } from './Hooks/useDynamicDimensions'
+export { default as useKeyPress } from './Hooks/useKeyPress'
+export { default as useStoreApi } from './Hooks/useStoreApi'
