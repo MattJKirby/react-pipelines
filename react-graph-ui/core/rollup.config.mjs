7f3eaf12694d075128795f3a98ee62bf66980cfd
Matt Kirby <MattJKirby@outlook.com>
2023-08-25 23:49:39 +0100

    Package things

--- react-graph-ui/core/rollup.config.mjs
+++ react-graph-ui/core/rollup.config.mjs
@@ -5,6 +5,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+import postcss from "rollup-plugin-postcss";
+
 import packageJson from "./package.json" assert { type: "json" };
 
 export default [
@@ -26,11 +28,13 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(), 
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
