52606a807470b17433e84774f9ef264c3ecb87fb
Matt Kirby <MattJKirby@outlook.com>
2023-08-29 23:38:58 +0100

    Handle will need a refactor at some point

--- react-graph-ui/core/src/Components/Handle/index.tsx
+++ react-graph-ui/core/src/Components/Handle/index.tsx
@@ -1,9 +1,17 @@
-import React, { useRef, memo } from "react"
+import { useRef, memo } from "react"
 import { useNodeContext } from "../../Contexts/NodeDataContext";
 import { HandleProps, INode } from "../../Types";
 import styles from '../../Styles/Handle/Handle.module.css'
 import InsertHelper from "./insertHelper";
 import { getUniqueHandleId } from "./utils";
+import { IGraphState } from "../../Types";
+import { useStore } from "../../Hooks/useStore";
+
+const selector = (s: IGraphState) => ({
+  selectedHandle: s.selectedHandle,
+  setSelectedHandle: s.setSelectedHandle,
+  addEdge: s.addEdge
+})
 
 /**
  * Node handle component
@@ -20,21 +28,48 @@ const Handle = ({
     const handleRef = useRef<HTMLDivElement>(null)
     const node = useNodeContext() as INode
     const handleId = getUniqueHandleId(node.id, id);
+    const { selectedHandle, setSelectedHandle, addEdge } = useStore(selector)
+
+    const handleSelectionInteraction = () => {
+      if (selectedHandle === null) {
+        setSelectedHandle({ sourceHandle: id, sourceNode: node.id, sourceHandleType: type, uniqueHandleId: handleId });
+      } else if (selectedHandle.uniqueHandleId === handleId) {
+        setSelectedHandle(null);
+      } else if (type !== selectedHandle.sourceHandleType) {
+        const sourceHandle = type === 'source' ? id : selectedHandle.sourceHandle;
+        const sourceNode = type === 'source' ? node.id : selectedHandle.sourceNode;
+        const targetHandle = type === 'target' ? id : selectedHandle.sourceHandle;
+        const targetNode = type === 'target' ? node.id : selectedHandle.sourceNode;
+        const edgeId = `edge-${sourceNode}_${sourceHandle}-${targetNode}_${targetHandle}`;
+    
+        addEdge([{ item: { id: edgeId, sourceNodeId: sourceNode, sourceNodeOutput: sourceHandle, targetNodeId: targetNode, targetNodeInput: targetHandle, type: 'default' } }]);
+        setSelectedHandle(null);
+      } else {
+        setSelectedHandle(null);
+      }
+    }
+
+    const isValidHandle = (): boolean => {
+      if(!selectedHandle){
+        return false
+      }
+
+      if(selectedHandle.sourceHandleType !== type){
+        return true
+      }
+      return false
+    }
     
     return (
-
       <div
-        className={`RP_Node__Handle RP_Node__Handle-${type} ${type} ${styles['RP_Node__Handle-' + position]} ${styles['RP_Node__Handle']}`}
+        className={`RP_Node__Handle RP_Node__Handle-${type} ${type} ${styles['RP_Node__Handle-' + position]} ${styles['RP_Node__Handle']} ${isValidHandle() || selectedHandle?.uniqueHandleId === handleId ? styles['RP_Node__Handle-connecting'] : ''}`}
         ref={handleRef}
-        style={{position: 'absolute'}}
         data-node-id={node.id}
-
         data-handle-id={handleId}
         data-id={`${handleId}-${type}`}
-
         data-handle-type={type}
         data-position={position}
-
+        onClick={() => handleSelectionInteraction()}
       >
 
           {handleRef.current && node.selected && type === 'source' &&
@@ -56,7 +91,7 @@ const Handle = ({
             </InsertHelper>
           }
 
-      </div>  
+      </div> 
     )
 }
 
