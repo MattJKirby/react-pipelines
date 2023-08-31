77da2cfc5c5c32c3fa9c74e50cbd5a2ce7b07137
Matt Kirby <MattJKirby@outlook.com>
2023-08-31 16:02:30 +0100

    Index

--- react-graph-ui/core/src/index.ts
+++ react-graph-ui/core/src/index.ts
@@ -1,8 +1,9 @@
 // Components
 export { default as GraphCanvas } from './Components/Canvas'
 export { default as Controls } from './Components/Controls'
-export { default as DefaultEdge } from './Components/Edge/DefaultEdge'
 export { default as RemovableEdge } from './Components/Edge/RemovableEdge'
+export { default as BaseEdge } from './Components/Edge/BaseEdge'
+export { default as BezierEdge } from './Components/Edge/BezierEdge'
 export { default as Graph } from './Components/Graph'
 export { default as GraphProvider } from './Components/GraphProvider'
 export { default as Handle } from './Components/Handle'
